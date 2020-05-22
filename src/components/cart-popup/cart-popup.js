2889600b7e7655a6cd63aaedaed02a5897e23173
vinsly <vinsly.bright@nagarro.com>
2020-05-23 00:50:13 +0530

    created card item to be shown in popup

--- src/components/cart-popup/cart-popup.js
+++ src/components/cart-popup/cart-popup.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
 import "./cart-popup.scss";
+import CartItem from "../cart-item/cart-item";
 
-const CartPopup = () => {
+const CartPopup = ({ cartItems }) => {
   return (
     <div className="cart-popup">
       <div className="cart-items">
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
       </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
-
-export default CartPopup;
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state.cart.cartItems,
+  };
+};
+export default connect(mapStateToProps)(CartPopup);
